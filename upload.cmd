6bf2f477993432cefea1557a4d8bfded54f7d495
ZhaoYF_Working <master@mail.amachi.com.cn>
2021-12-05 14:54:01 +0800

    first, 2021/12/05 周日, 14:54:00.80

--- upload.cmd
+++ upload.cmd
@@ -1,7 +1,17 @@
 @echo off
 
-git add .
-git commit -m "%date%, %time%"
-git push
+if not exist ".cmd" (
+    git init
+    git add .
+    git commit -m "first, %date%, %time%"
+    git branch -M master
+    git remote add origin git@github.com:AmachiInori/FF14-Config.git
+    git push -u origin master
+    git push
+) else (
+    git add .
+    git commit -m "%date%, %time%"
+    git push
+)
 
 pause
